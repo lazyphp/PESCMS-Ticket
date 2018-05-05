ff5d520d30f96cd6dc7896f71759e35fa3b8f96a
luoboss <dev@pescms.com>
2018-05-05 22:58:58 +0800

    修正配置文件

--- Public/Install/Config/config_same.php
+++ Public/Install/Config/config_same.php
@@ -3,6 +3,6 @@
 return array(
     'APP_GROUP_LIST' => 'Install', //项目分组设定
     'DEFAULT_GROUP' => 'Install', //默认分组
-    'ERROR_PROMPT' => '/Core/Theme/error.php',
+    'SESSION_ID' => 'PESTKSESSION',
     'LOG_PATH' => '/log',
 );

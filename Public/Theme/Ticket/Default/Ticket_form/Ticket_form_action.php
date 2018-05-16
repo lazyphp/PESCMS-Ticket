cb5293cc2d3d97772c67af08e8d58cc789bbb146
luoboss <dev@pescms.com>
2018-05-16 22:23:28 +0800

    优化模板

--- Public/Theme/Ticket/Default/Ticket_form/Ticket_form_action.php
+++ Public/Theme/Ticket/Default/Ticket_form/Ticket_form_action.php
@@ -6,55 +6,54 @@
 
 <?php foreach ($field as $key => $value) : ?>
     <?php if ($value['field_form']): ?>
-        <li>
-            <div class="am-g">
-                <label for="doc-ipt-pwd-2" class="am-u-sm-2 am-form-label"><?= $value['field_display_name'] ?></label>
+		<div class="am-g am-g-collapse">
+			<div class="am-u-sm-12 am-u-sm-centered">
+				<div class="am-form-group">
+					<label class="am-block"><?= $value['field_display_name'] ?><?= $value['field_required'] == '1' ? '<i class="am-text-danger">*</i>' : '' ?></label>
 
-                <div class="am-u-sm-9">
-                    <?php if ($value['field_name'] == 'type'): ?>
-                        <?php $value['field_option'] = json_encode($formType); ?>
-                    <?php elseif ($value['field_name'] == 'verify'): ?>
-                        <?php $value['field_option'] = json_encode($checkType); ?>
-                    <?php elseif ($value['field_name'] == 'bind'): ?>
-                        <?php $value['field_option'] = json_encode($bind); ?>
-                    <?php elseif ($value['field_name'] == 'option' && !empty($value['value'])): ?>
-                        <?php
-                        $optionStr = '';
-                        foreach (json_decode(htmlspecialchars_decode($value['value']), true) as $ok => $ov) {
-                            $optionStr .= "{$ok}|{$ov}\n";
-                        }
-                        $value['value'] = $optionStr;
-                        ?>
-                    <?php endif; ?>
+					<?php if ($value['field_name'] == 'type'): ?>
+						<?php $value['field_option'] = json_encode($formType); ?>
+					<?php elseif ($value['field_name'] == 'verify'): ?>
+						<?php $value['field_option'] = json_encode($checkType); ?>
+					<?php elseif ($value['field_name'] == 'bind'): ?>
+						<?php $value['field_option'] = json_encode($bind); ?>
+					<?php elseif ($value['field_name'] == 'option' && !empty($value['value'])): ?>
+						<?php
+						$optionStr = '';
+						foreach (json_decode(htmlspecialchars_decode($value['value']), true) as $ok => $ov) {
+							$optionStr .= "{$ok}|{$ov}\n";
+						}
+						$value['value'] = $optionStr;
+						?>
+					<?php endif; ?>
 
-                    <?php if ($value['field_name'] == 'bind_value'): ?>
-                        <span id="bind_value">
-                        <?php if (empty($ticket_form_bind)): ?>
-                            待选择
-                        <?php else: ?>
-                            <?php foreach ($bindValue[$ticket_form_bind] as $bk => $bv): ?>
-                                <?php $bindValueSelect = in_array($bv, explode(',', $ticket_form_bind_value)) ? 'checked="checked"' : '' ?>
-                                <label class="am-checkbox-inline">
-                                    <input type="checkbox" value="<?= $bv ?>"
-                                           name="bind_value[]" <?= $bindValueSelect ?>> <?= $bk ?>
-                                </label>
-                            <?php endforeach; ?>
+					<?php if ($value['field_name'] == 'bind_value'): ?>
+						<span id="bind_value">
+						<?php if (empty($ticket_form_bind)): ?>
+							待选择
+						<?php else: ?>
+							<?php foreach ($bindValue[$ticket_form_bind] as $bk => $bv): ?>
+								<?php $bindValueSelect = in_array($bv, explode(',', $ticket_form_bind_value)) ? 'checked="checked"' : '' ?>
+								<label class="am-checkbox-inline">
+									<input type="checkbox" value="<?= $bv ?>"
+									       name="bind_value[]" <?= $bindValueSelect ?>> <?= $bk ?>
+								</label>
+							<?php endforeach; ?>
                         </span>
-                        <?php endif; ?>
+						<?php endif; ?>
 
-                    <?php else: ?>
-                        <?= $form->formList($value); ?>
-                    <?php endif; ?>
-                    <?php if (!empty($value['field_explain'])): ?>
-                        <div class="am-alert am-alert-secondary am-text-xs " data-am-alert>
-                            <i class="am-icon-lightbulb-o"></i> <?= $value['field_explain'] ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div
-                    class="am-u-sm-1"><?= $value['field_required'] == '1' ? '<span class="am-badge am-round am-badge-danger">必填</span>' : '' ?></div>
-            </div>
-        </li>
+					<?php else: ?>
+						<?= $form->formList($value); ?>
+					<?php endif; ?>
+
+					<?php if (!empty($value['field_explain'])): ?>
+						<div class="am-alert am-alert-secondary am-text-xs " data-am-alert>
+							<i class="am-icon-lightbulb-o"></i> <?= $value['field_explain'] ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
     <?php endif; ?>
 <?php endforeach; ?>
 

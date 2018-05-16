72294066572a211ed80e49eaa73b64df29d8b787
luoboss <dev@pescms.com>
2018-05-16 10:16:52 +0800

    修正安装错误

--- Public/Install/Theme/Install/Default/Index/Index_option.php
+++ Public/Install/Theme/Install/Default/Index/Index_option.php
@@ -3,44 +3,43 @@
     <input type="hidden" name="method" value="GET"/>
 
     <div class="am-form-group">
-        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">程序名称:</label>
-
+        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">网站域名:</label>
         <div class="am-u-sm-10">
-            <input type="text" name="sitetitle" placeholder="程序名称" required>
+            <input type="text" name="domain" placeholder="网站域名" required>
         </div>
     </div>
 
-            <div class="am-form-group">
-                <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员帐号:</label>
-                <div class="am-u-sm-10">
-                    <input type="text" name="account" placeholder="管理员帐号" required>
-                </div>
-            </div>
+    <div class="am-form-group">
+        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员帐号:</label>
+        <div class="am-u-sm-10">
+            <input type="text" name="account" placeholder="管理员帐号" required>
+        </div>
+    </div>
 
-            <div class="am-form-group">
-                <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员密码:</label>
-                <div class="am-u-sm-10">
-                    <input type="text" name="passwd" placeholder="管理员密码" required>
-                </div>
-            </div>
+    <div class="am-form-group">
+        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员密码:</label>
+        <div class="am-u-sm-10">
+            <input type="text" name="passwd" placeholder="管理员密码" required>
+        </div>
+    </div>
 
-            <div class="am-form-group">
-                <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员邮箱:</label>
-                <div class="am-u-sm-10">
-                    <input type="text" name="mail" placeholder="管理员邮箱" required>
-                </div>
-            </div>
+    <div class="am-form-group">
+        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员邮箱:</label>
+        <div class="am-u-sm-10">
+            <input type="text" name="mail" placeholder="管理员邮箱" required>
+        </div>
+    </div>
 
-            <div class="am-form-group">
-                <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员名字:</label>
-                <div class="am-u-sm-10">
-                    <input type="text" name="name"  placeholder="管理员名字" required>
-                </div>
-            </div>
+    <div class="am-form-group">
+        <label for="doc-ipt-3" class="am-u-sm-2 am-form-label">管理员名字:</label>
+        <div class="am-u-sm-10">
+            <input type="text" name="name" placeholder="管理员名字" required>
+        </div>
+    </div>
 
-            <div class="am-margin-top am-fl">
-                <a href="<?=DOCUMENT_ROOT?>/?m=Index&a=config" class="am-btn am-btn-default">上一步</a> 
-            </div>
+    <div class="am-margin-top am-fl">
+        <a href="<?= DOCUMENT_ROOT ?>/?m=Index&a=config" class="am-btn am-btn-default">上一步</a>
+    </div>
 
     <div class="am-margin-top am-fr">
         <button type="submit" id="next" class="am-btn am-btn-default">下一步</button>

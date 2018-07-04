c7b30c4fb58c420d171d389124385bfd277b4a81
luoboss <dev@pescms.com>
2018-07-04 22:09:38 +0800

    修正后台管理在手机模式下，无法呼出菜单。

--- Public/Theme/Ticket/Default/Topbar.php
+++ Public/Theme/Ticket/Default/Topbar.php
@@ -4,10 +4,10 @@
     </h1>
 
     <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only"
-            data-am-collapse="{target: '#doc-topbar-collapse'}">
+            data-am-collapse="{target: '#ticket-topbar-collapse'}">
         <span class="am-sr-only">导航切换</span> <span class="am-icon-bars"></span></button>
 
-    <div class="am-collapse am-topbar-collapse">
+    <div class="am-collapse am-topbar-collapse" id="ticket-topbar-collapse">
         <ul class="am-nav am-nav-pills am-topbar-nav">
             <?php foreach ($menu as $topValue): ?>
                 <?php if (!empty($topValue['menu_child'])): ?>

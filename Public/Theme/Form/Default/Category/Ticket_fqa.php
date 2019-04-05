df346d0425fc1c86c1731a18383e2aa7297aac91
luoboss <dev@pescms.com>
2019-04-06 00:34:44 +0800

    优化工单列表在FQA弹出后"创建工单"按钮的位置。

--- Public/Theme/Form/Default/Category/Ticket_fqa.php
+++ Public/Theme/Form/Default/Category/Ticket_fqa.php
@@ -1,9 +1,15 @@
 <div class="am-panel am-panel-default ticket-fqa" style="display: none">
     <div class="am-panel-bd am-margin-bottom">
         <span class="am-fl"><strong>常见问题</strong></span>
-        <a href="javascript:;" class="am-fr am-text-danger am-animation-shake">创建工单>></a>
     </div>
     <ol class="fqa-list am-avg-sm-1 am-avg-lg-3">
-        <li><a href=""><p>努力加载中...</p></li>
+        <li><a href="javascript:;"><p>努力加载中...</p></a></li>
     </ol>
+
+    <ul>
+        <li>
+            <a href="javascript:;" class="am-btn am-btn-danger am-btn-sm am-radius am-animation-shake">创建工单</a>
+        </li>
+    </ul>
+
 </div>

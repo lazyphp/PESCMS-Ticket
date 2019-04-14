6d324172e01f6ad74de14651162e4550939e7b2b
luoboss <dev@pescms.com>
2019-04-15 00:20:23 +0800

    编写新的前台界面

--- Public/Theme/Form/Default/footer.php
+++ Public/Theme/Form/Default/footer.php
@@ -1,12 +1,48 @@
 <?php
 /**
  * 非授权用户请勿删除程序版权信息，若需要删除请购买授权。
+ * PESCMS将对非授权的用户，删除版权的用户保留追究法律责任的权利
  * 授权说明: https://www.pescms.com/Page/Authorization.html
  */
 ?>
 <footer class="my-footer pescms-footer-<?= $system['notice_way'] ?>">
-    <small>© Copyright 2015-<?= date('Y') ?>. Power by <a href="//www.pescms.com" target="_blank">PESCMS Ticket</a>
-    </small>
+    <!--这部分关于PESCMS产品简介的可以自行删除-->
+    <div class="am-g pescms-introduce">
+        <div class="am-u-sm-8 am-u-sm-centered">
+            <div class="am-g">
+                <div class="am-u-sm-3">
+                    <h4>关于PESCMS</h4>
+                    <p>PESCMS旗下有TEAM团队任务管理系统，Ticket客服工单系统，DOC文档管理系统以及基于浏览器开发的基金定投助手。</p>
+                </div>
+                <div class="am-u-sm-3">
+                    <h4>产品列表</h4>
+                    <ul>
+                        <li><a href="https://www.pescms.com/download/2.html">PESCMS TEAM 团队任务管理系统</a></li>
+                        <li><a href="https://www.pescms.com/download/3.html">PESCMS DOC 文档管理系统</a></li>
+                        <li><a href="https://www.pescms.com/download/5.html">PESCMS TICKET 客服工单系统</a></li>
+                        <li><a href="https://www.pescms.com/download/4.html">PESCMS LOGIN 网站登录管理</a></li>
+                        <li><a href="https://www.pescms.com/download/6.html">基金定投助手</a></li>
+                    </ul>
+                </div>
+                <div class="am-u-sm-4 am-u-end">
+                    <h4>产品服务</h4>
+                    <ul>
+                        <li><a href="https://www.pescms.com/Page/Authorization.html">商业授权</a></li>
+                        <li><a href="https://www.pescms.com/Authorize-Verify">授权查询</a></li>
+                        <li><a href="https://www.pescms.com/service.html">有偿服务</a></li>
+                        <li><a href="https://www.pescms.com/Page/ad.html">广告投放</a></li>
+                        <li><a href="https://www.pescms.com/Page/donate.html">赞助捐赠</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--这部分关于PESCMS产品简介的可以自行删除-->
+
+    <div class="copyright am-text-center">
+        <small>© Copyright 2015-<?= date('Y') ?>. Power by <a href="//www.pescms.com" target="_blank">PESCMS Ticket</a>
+        </small>
+    </div>
 </footer>
 </body>
 </html>

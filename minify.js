39361bef34a68e181a7649c35abf1ce91d27538f
luoboss <dev@pescms.com>
2020-03-25 23:11:52 +0800

    完善压缩脚本

--- minify.js
+++ minify.js
@@ -3,10 +3,14 @@ var uglify = require('uglify-js');
 var CleanCSS = require('clean-css');
 
 //js文件压缩方法
-//js文件压缩方法
 function jsMinify(flieIn, fileOut) {
-    var flieIn=Array.isArray(flieIn)? flieIn : [flieIn];
-    var result = uglify.minify(flieIn);
+    //保留注释
+    var options = {
+        output: {
+            comments: /^!/
+        }
+    };
+    var result = uglify.minify(fs.readFileSync(flieIn, "utf8"), options);
     fs.writeFileSync(fileOut, result.code, 'utf8');
 }
 
@@ -18,6 +22,7 @@ function cssMinify(flieIn, fileOut) {
     })
 }
 
-jsMinify(['./app.js'], './ccc.min.js');
+
+// jsMinify('./app.js', './ccc.min.js');
 
 // cssMinify(['./admin.css'], './admin.min.css');

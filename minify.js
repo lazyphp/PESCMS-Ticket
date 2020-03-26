b3cc3d2f2d1d16ac2d2ba1490fc4d276419b1d06
luoboss <dev@pescms.com>
2020-03-26 09:47:58 +0800

    优化静态资源，提升加载速度。

--- minify.js
+++ minify.js
@@ -22,7 +22,23 @@ function cssMinify(flieIn, fileOut) {
     })
 }
 
+//开始压缩JS资源
+jsMinify('./Public/Theme/assets/js/spectrum.js', './Public/Theme/assets/js/spectrum.min.js');
+jsMinify('./Public/Theme/assets/js/webuploader.js', './Public/Theme/assets/js/webuploader.min.js');
+jsMinify('./Public/Theme/assets/js/AMUIwebuploader.js', './Public/Theme/assets/js/AMUIwebuploader.min.js');
+jsMinify('./Public/Theme/assets/js/app.js', './Public/Theme/assets/js/app.min.js');
+jsMinify('./Public/Theme/assets/js/ticket.js', './Public/Theme/assets/js/ticket.min.js');
 
-// jsMinify('./app.js', './ccc.min.js');
+//百度编辑器
+jsMinify('./Public/Theme/assets/ueditor/ueditor.config.js', './Public/Theme/assets/ueditor/ueditor.config.min.js');
+jsMinify('./Public/Theme/assets/ueditor/ueditor_ticket.config.js', './Public/Theme/assets/ueditor/ueditor_ticket.config.min.js');
+jsMinify('./Public/Theme/assets/ueditor/ueditor.all.js', './Public/Theme/assets/ueditor/ueditor.all.min.js');
+jsMinify('./Public/Theme/assets/ueditor/lang/zh-cn/zh-cn.js', './Public/Theme/assets/ueditor/lang/zh-cn/zh-cn.min.js');
 
-// cssMinify(['./admin.css'], './admin.min.css');
+//开始压缩CSS资源
+cssMinify(['./Public/Theme/assets/css/app.css'], './Public/Theme/assets/css/app.min.css');
+cssMinify(['./Public/Theme/assets/css/index.css'], './Public/Theme/assets/css/index.min.css');
+cssMinify(['./Public/Theme/assets/css/ui-dialog.css'], './Public/Theme/assets/css/ui-dialog.min.css');
+cssMinify(['./Public/Theme/assets/css/webuploader.css'], './Public/Theme/assets/css/webuploader.min.css');
+cssMinify(['./Public/Theme/assets/css/ticket.css'], './Public/Theme/assets/css/ticket.min.css');
+cssMinify(['./Public/Theme/assets/css/spectrum.css'], './Public/Theme/assets/css/spectrum.min.css');

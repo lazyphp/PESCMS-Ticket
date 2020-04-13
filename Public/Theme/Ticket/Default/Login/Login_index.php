354bee3cf73984eb4b591278af7a943dac37533b
luoboss <dev@pescms.com>
2020-04-13 15:09:45 +0800

    优化后台登录页样式。

--- Public/Theme/Ticket/Default/Login/Login_index.php
+++ Public/Theme/Ticket/Default/Login/Login_index.php
@@ -1,43 +1,49 @@
 <?php include THEME_PATH.'/header.php' ?>
 <div class="am-g " style="padding-top: 8rem;">
     <div class="am-u-lg-4 am-u-sm-12  am-u-lg-centered am-u-sm-centered">
-        <form action="" class="ajax-submit" method="POST" data-am-validator>
-            <?= $label->token() ?>
-            <input type="hidden" name="back_url" value="<?= $_GET['back_url']; ?>"/>
-            <h1 class="am-text-center am-text-xxxl" style="color: #fff"><?= $system['siteTitle'] ?></h1>
+        <div class="am-panel am-panel-default" style="background: rgba(255, 255, 255, 0.55);margin-top: 12rem">
+            <div class="am-panel-bd">
+                <form action="" class="ajax-submit" method="POST" data-am-validator>
+                    <?= $label->token() ?>
+                    <input type="hidden" name="back_url" value="<?= $_GET['back_url']; ?>"/>
+                    <h1 class="am-text-center am-margin-top-0" ><?= $system['siteTitle'] ?></h1>
 
-            <div class="am-input-group am-margin-bottom">
-                <span class="am-input-group-label"><i class="am-icon-user am-icon-fw"></i></span>
-                <input name="account" class="am-form-field" type="text" placeholder="账号" autofocus required>
-            </div>
-            <div class="am-input-group am-margin-bottom">
-                <span class="am-input-group-label"><i class="am-icon-lock am-icon-fw"></i></span>
-                <input name="passwd" class="am-form-field" type="password" placeholder="密码" required>
-            </div>
-            
-            <?php if(json_decode($system['login_verify'])[1] == '2'): ?>
-            <div class="am-input-group am-margin-bottom">
-                <span class="am-input-group-label"><i class="am-icon-shield am-icon-fw"></i></span>
-                <input type="text" class="am-form-field" name="verify" maxlength="<?= $system['verifyLength'] ?>">
-                <span class="am-input-group-btn">
+                    <div class="am-input-group am-margin-bottom">
+                        <span class="am-input-group-label"><i class="am-icon-user am-icon-fw"></i></span>
+                        <input name="account" class="am-form-field" type="text" placeholder="账号" autofocus required>
+                    </div>
+                    <div class="am-input-group am-margin-bottom">
+                        <span class="am-input-group-label"><i class="am-icon-lock am-icon-fw"></i></span>
+                        <input name="passwd" class="am-form-field" type="password" placeholder="密码" required>
+                    </div>
+
+                    <?php if(json_decode($system['login_verify'])[1] == '2'): ?>
+                        <div class="am-input-group am-margin-bottom">
+                            <span class="am-input-group-label"><i class="am-icon-shield am-icon-fw"></i></span>
+                            <input type="text" class="am-form-field" name="verify" maxlength="<?= $system['verifyLength'] ?>">
+                            <span class="am-input-group-btn">
                     <img src="<?= $label->url('Index-verify') ?>" class="refresh-verify am-padding-left-sm">
                 </span>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="am-form-group am-form-group-sm">
+                        <div class="am-checkbox">
+                            <label>
+                                <input type="checkbox" class="remember-password" value="1"> 记住登录信息
+                            </label>
+                        </div>
+                    </div>
+
+                    <button class="am-btn am-btn-primary am-btn-block">登录</button>
+
+                    <?= $label->loginEvent(); ?>
+
+                </form>
             </div>
-            <?php endif; ?>
+        </div>
 
-            <div class="am-form-group am-form-group-sm">
-                <div class="am-checkbox">
-                    <label>
-                        <input type="checkbox" class="remember-password" value="1"> 记住登录信息
-                    </label>
-                </div>
-            </div>
 
-            <button class="am-btn am-btn-primary am-btn-block">登录</button>
-
-            <?= $label->loginEvent(); ?>
-            
-        </form>
     </div>
 </div>
 <style>

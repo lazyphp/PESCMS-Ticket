44fc2f00be134918725dfb1c1f285a00df914db4
luoboss <dev@pescms.com>
2020-04-15 15:34:32 +0800

    开始编写阿里云短信接口。

--- Public/Theme/Ticket/Default/Setting/action/sms.php
+++ Public/Theme/Ticket/Default/Setting/action/sms.php
@@ -1,7 +1,21 @@
 <div class="am-panel am-panel-default">
     <div class="am-panel-hd">短信通知</div>
     <div class="am-panel-bd">
+
         <div class="am-g am-g-collapse">
+            <div class="am-u-sm-12 am-u-sm-centered">
+                <div class="am-form-group">
+                    <label class="am-block">业务提供商</label>
+                    <select name="sms[COMPANY]" required>
+                        <option value="">请选择短信业务提供商</option>
+                        <option value="1">阿里云</option>
+                        <option value="2">互亿无线</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="am-g am-g-collapse sms-2">
             <div class="am-u-sm-12 am-u-sm-centered">
                 <div class="am-form-group">
                     <label class="am-block">APIID</label>
@@ -10,7 +24,7 @@
             </div>
         </div>
 
-        <div class="am-g am-g-collapse">
+        <div class="am-g am-g-collapse sms-2">
             <div class="am-u-sm-12 am-u-sm-centered">
                 <div class="am-form-group">
                     <label class="am-block">APIKEY</label>
@@ -19,7 +33,7 @@
             </div>
         </div>
 
-        <div class="am-alert am-alert-secondary am-text-xs " data-am-alert>
+        <div class="am-alert am-alert-secondary am-text-xs sms-2" data-am-alert>
             <i class="am-icon-lightbulb-o"></i> 使用短信业务，需要先进行注册：<a href="https://www.pescms.com/?m=Index&a=sms" target="_blank" style="color:#0e90d2 "><b>点击注册</b></a>。短信的设置教程:<a href="https://www.pescms.com/d/v/1.2.7/22/145.html" target="_blank" style="color:#0e90d2 "> <b>教程</b></a>
         </div>
 
@@ -40,3 +54,14 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(function () {
+
+        $('select[name="sms[COMPANY]"]').on('click', function () {
+            var value = $(this).val();
+            $("div[class*='sms-']").hide();
+            $('.sms-'+value).show();
+        })
+    })
+</script>

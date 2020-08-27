c11436328e159a1bf2c54aa23dea7cf3da2ffb4f
luoboss <dev@pescms.com>
2020-08-27 14:38:02 +0800

    优化后台通知消息时生成的登陆链接。

--- Model/Option.php
+++ Model/Option.php
@@ -10,4 +10,28 @@ class Option extends \Core\Model\Model {
         return json_decode($content['value'], true);
     }
 
+    /**
+     * 获取后台登录参数
+     * @return mixed|string
+     */
+    public static function getNoticeLoginParam(){
+        $loginParam = explode('|', \Core\Func\CoreFunc::$param['system']['notice_login']);
+
+        if($loginParam[0] < time() - 86400){
+            static $newLoginParam;
+            if(empty($newLoginParam)){
+                $newLoginParam = \Model\Extra::getOnlyNumber();
+            }
+            self::db('option')->where('option_name = :option_name')->update([
+                'noset' => [
+                    'option_name' => 'notice_login'
+                ],
+                'value' => time() . "|{$newLoginParam}"
+            ]);
+            return $newLoginParam;
+        }else{
+            return $loginParam['1'];
+        }
+    }
+
 }
--- Upgrade/sql/1.3.4.sql
+++ Upgrade/sql/1.3.4.sql
@@ -1,1 +1,3 @@
 INSERT INTO `pes_option` (`id`, `option_name`, `name`, `value`, `option_range`) VALUES (NULL, 'rowlock', '开启行锁', '0', 'system');
+
+INSERT INTO `pes_option` (`id`, `option_name`, `name`, `value`, `option_range`) VALUES (NULL, 'notice_login', '登录参数', '', 'system');

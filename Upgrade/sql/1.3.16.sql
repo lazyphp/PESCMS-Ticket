3b70b43ed8f1a6d9cbadb0402b4258b57ff65c9f
luoboss <dev@pescms.com>
2022-07-07 15:56:04 +0800

    补充新版的升级SQL。 优化升级SQL的内容。

--- Upgrade/sql/1.3.16.sql
+++ Upgrade/sql/1.3.16.sql
@@ -1,4 +1,4 @@
-UPDATE `pes_field` SET `field_explain` = '1. 默认留空单号规则随机生成。2. 只填写{X}则用雪花ID规则。3. 关键词{Y}是年，{M}是月，{D}是日，{Z}是当前工单模型提交的工单总数量，{A}是今天工单模型提交工单数量，{S}是五位数的随机值。' WHERE `pes_field`.`field_id` = 283;
+UPDATE `pes_field` SET `field_explain` = '1. 默认留空单号规则随机生成。2. 只填写{X}则用雪花ID规则。3. 关键词{Y}是年，{M}是月，{D}是日，{Z}是当前工单模型提交的工单总数量，{A}是今天工单模型提交工单数量，{S}是五位数的随机值。' WHERE `field_model_id` = 15 AND `field_name` = 'custom_no';
 
 INSERT INTO `pes_field` (`field_id`, `field_model_id`, `field_name`, `field_display_name`, `field_type`, `field_option`, `field_explain`, `field_default`, `field_required`, `field_listsort`, `field_list`, `field_form`, `field_status`, `field_is_null`, `field_only`, `field_action`) VALUES
 (NULL, 15, 'fqa_tips', 'FQA指引', 'radio', '{&quot;\\u5f00\\u542f&quot;:&quot;0&quot;,&quot;\\u5173\\u95ed&quot;:&quot;1&quot;}', '默认开启FQA指引，客户提交工单时，若当前工单模型存在FQA文档，则弹出FQA指引列表和工单提交按钮。', '0', 1, 7, 0, 1, 1, 0, 0, 'POST,PUT');
@@ -18,9 +18,16 @@ ALTER TABLE `pes_ticket_model` ADD `ticket_model_fqa_tips` TINYINT(1) NOT NULL C
 ALTER TABLE `pes_user` ADD `user_suspension_button` TINYINT(1) NOT NULL COMMENT '是否关闭悬浮按钮' AFTER `user_browser_msg_time`;
 
 UPDATE `pes_field` SET `field_option` = '{&quot;\\u663e\\u793a&quot;:&quot;1&quot;,&quot;\\u9690\\u85cf&quot;:&quot;0&quot;,&quot;\\u4ec5\\u540e\\u53f0&quot;:&quot;2&quot;}' WHERE `pes_field`.`field_id` = 15;
-UPDATE `pes_field` SET `field_form` = '2' WHERE `pes_field`.`field_id` = 235;
-UPDATE `pes_field` SET `field_form` = '2' WHERE `pes_field`.`field_id` = 270;
-UPDATE `pes_field` SET `field_form` = '2' WHERE `pes_field`.`field_id` = 273;
-UPDATE `pes_field` SET `field_form` = '2' WHERE `pes_field`.`field_id` = 281;
+
+INSERT INTO `pes_field` (`field_id`, `field_model_id`, `field_name`, `field_display_name`, `field_type`, `field_option`, `field_explain`, `field_default`, `field_required`, `field_listsort`, `field_list`, `field_form`, `field_status`, `field_is_null`, `field_only`, `field_action`) VALUES (NULL, '20', 'dingtalk', '钉钉', 'text', '', '', '', '0', '32', '0', '2', '1', '1', '0', 'POST,PUT');
+
+INSERT INTO `pes_field` (`field_id`, `field_model_id`, `field_name`, `field_display_name`, `field_type`, `field_option`, `field_explain`, `field_default`, `field_required`, `field_listsort`, `field_list`, `field_form`, `field_status`, `field_is_null`, `field_only`, `field_action`) VALUES (NULL, '20', 'wxWork', '企业微信', 'text', '', '', '', '0', '33', '0', '2', '1', '1', '0', 'POST,PUT');
+
+
+UPDATE `pes_field` SET `field_form` = '2' WHERE `field_model_id` = 20 AND `field_name` = 'weixin';
+UPDATE `pes_field` SET `field_form` = '2' WHERE `field_model_id` = 20 AND `field_name` = 'wxapp';
+UPDATE `pes_field` SET `field_form` = '2' WHERE `field_model_id` = 20 AND `field_name` = 'wxWork';
+UPDATE `pes_field` SET `field_form` = '2' WHERE `field_model_id` = 20 AND `field_name` = 'dingtalk';
+UPDATE `pes_field` SET `field_form` = '2' WHERE `field_model_id` = 20 AND `field_name` = 'requisition';
 
 UPDATE `pes_field` SET `field_option` = '{&quot;\\u5f85\\u89e3\\u51b3&quot;:&quot;0&quot;,&quot;\\u53d7\\u7406\\u4e2d&quot;:&quot;1&quot;,&quot;\\u5f85\\u56de\\u590d&quot;:&quot;2&quot;}' WHERE `pes_field`.`field_id` = 269;

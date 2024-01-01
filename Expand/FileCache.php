b89737daca6dc7b38a1cf72a9d35518106f15c6c
luoboss <dev@pescms.com>
2024-01-01 14:08:01 +0800

    优化微信登录逻辑。

--- Expand/FileCache.php
+++ Expand/FileCache.php
@@ -70,6 +70,11 @@ class FileCache {
         }
     }
 
+    /**
+     * 清除缓存文件
+     * @param $fileName
+     * @return void
+     */
     public function clearCache($fileName){
         $cacheFile = sprintf($this->cacheFileName, $fileName);
         if(is_file($cacheFile)){
